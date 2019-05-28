16d30e6df7604521aeb9e8f8a9d6da131a9c7d8f
elecyb <elecyb@hotmail.com>
2019-05-27 23:48:45 -0300

    Fix menu item text

--- Forms/MainForm.Designer.cs
+++ Forms/MainForm.Designer.cs
@@ -527,28 +527,28 @@
             // 
             this.displayQuickBarToolStripMenuItem.CheckOnClick = true;
             this.displayQuickBarToolStripMenuItem.Name = "displayQuickBarToolStripMenuItem";
-            this.displayQuickBarToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
-            this.displayQuickBarToolStripMenuItem.Text = "Display Quick Bar";
+            this.displayQuickBarToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.displayQuickBarToolStripMenuItem.Text = "Hide Quick Bar";
             this.displayQuickBarToolStripMenuItem.Click += new System.EventHandler(this.tsmiDisplayQuickBar_Click);
             // 
             // viewLogToolStripMenuItem
             // 
             this.viewLogToolStripMenuItem.Image = global::OSPE.Properties.Resources.File_info16;
             this.viewLogToolStripMenuItem.Name = "viewLogToolStripMenuItem";
-            this.viewLogToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.viewLogToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
             this.viewLogToolStripMenuItem.Text = "Open Log";
             this.viewLogToolStripMenuItem.Click += new System.EventHandler(this.tsmiViewLog_Click);
             // 
             // toolStripSeparator11
             // 
             this.toolStripSeparator11.Name = "toolStripSeparator11";
-            this.toolStripSeparator11.Size = new System.Drawing.Size(163, 6);
+            this.toolStripSeparator11.Size = new System.Drawing.Size(150, 6);
             // 
             // tsmiMenuConfigure
             // 
             this.tsmiMenuConfigure.Image = global::OSPE.Properties.Resources.Maintenance16;
             this.tsmiMenuConfigure.Name = "tsmiMenuConfigure";
-            this.tsmiMenuConfigure.Size = new System.Drawing.Size(166, 22);
+            this.tsmiMenuConfigure.Size = new System.Drawing.Size(153, 22);
             this.tsmiMenuConfigure.Text = "Configure";
             this.tsmiMenuConfigure.Click += new System.EventHandler(this.tsmiConfigure_Click);
             // 
